610266023eb3a085b47d22ec4ab6994deeb89aea
sama <sama.mazz93@gmail.com>
2021-06-14 19:15:39 +0300

    changes

--- part_B/Character.h
+++ part_B/Character.h
@@ -12,7 +12,6 @@ protected:
 
 protected:
     bool canMove(const GridPoint& src, const GridPoint& dst); // raghad
-
     bool canAttack(CharacterCoordinates src_character_coordinates, CharacterCoordinates dst_character_coordinates);
     virtual bool attackInRange(CharacterCoordinates src_character_coordinates, CharacterCoordinates dst_character_coordinates) = 0;
     virtual bool isOutOfAmmo(CharacterCoordinates src_character_coordinates, CharacterCoordinates dst_character_coordinates) = 0;
@@ -22,4 +21,8 @@ protected:
     void reload(); // raghad
 
 public:
+    Character(Team team, units_t health, units_t ammo, units_t range, units_t power);
+    ~Character() = default;
+    Character(const Character&) = default;
+    Character& operator=(const Character&) = default;
     clone();
