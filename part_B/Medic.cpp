15b91950d21d76f231d30c4cbf43be08eb0436c4
Sama Mazzawi <sama.mazz93@gmail.com>
2021-06-15 15:30:48 +0300

    Update Medic.cpp

--- part_B/Medic.cpp
+++ part_B/Medic.cpp
@@ -1,22 +1,25 @@
+#include "Medic.h"
 
-bool Medic::attackInRange(GridPoint src_grid_point, GridPoint dst_grid_point) override {
-    if (GridPoint::distance(src_grid_point, dst_grid_point) > range) {
+using namespace mtm;
+
+bool Medic::attackInRange(GridPoint dst_grid_point) override {
+    if (GridPoint::distance(coordinates, dst_grid_point) > range) {
         return false;
     }
     return true;
 }
 
 
-bool Medic::isOutOfAmmo(CharacterCoordinates dst_character_coordinates) override {
-    if (dst_character_coordinates->character->team != this->team) {
-        return ammo < 1;
+bool Medic::enoughAmmo(Team dst_team) override {
+    if (dst_team == this->team()) {
+        return true;
     }
-    return false;
+    return ammo >= 1;
 }
 
 
-bool Medic::legalTarget(CharacterCoordinates dst_character_coordinates) override {
-    if (dst_character_coordinates->character == NULL) {
+bool Medic::legalTarget(Team dst_team) override {
+    if (dst_team == EMPTY_CELL) {
         return false;
     }
     return true;
