15dc00e3e0b0a48f34419dc1e875db3f39fa4768
Sama Mazzawi <sama.mazz93@gmail.com>
2021-06-15 15:20:11 +0300

    Update Character.h

--- part_B/Character.h
+++ part_B/Character.h
@@ -1,28 +1,32 @@
+#include <memory>
 
+#include <Auxiliaries.h>
 
 // abstract class
 
-class Character:
+class Character {
 protected:
-    Team team;
-    units_t health;
-    units_t ammo;
-    units_t range;
-    units_t power;
+    mtm::Team team;
+    mtm::units_t health;
+    mtm::units_t ammo;
+    mtm::units_t range;
+    mtm::units_t power;
+    mtm::GridPoint coordinates;
 
 protected:
-    bool canMove(const GridPoint& src, const GridPoint& dst); // raghad
-    bool canAttack(CharacterCoordinates src_character_coordinates, CharacterCoordinates dst_character_coordinates);
-    virtual bool attackInRange(GridPoint src_grid_point, GridPoint dst_grid_point) = 0;
-    virtual bool isOutOfAmmo(CharacterCoordinates dst_character_coordinates) = 0;
-    virtual bool legalTarget(CharacterCoordinates dst_character_coordinates) = 0;
+    bool canMove(const mtm::GridPoint& src, const mtm::GridPoint& dst);
+    bool canAttack(std::shared_ptr<Character> dst_character, std::shared_ptr<mtm::GridPoint> dst_coordinates);
+    virtual bool attackInRange(std::shared_ptr<mtm::GridPoint> dst_grid_point) = 0;
+    virtual bool enoughAmmo(mtm::Team dst_character_team) = 0;
+    virtual bool legalTarget(mtm::Team dst_character_team) = 0;
     virtual void updateAmmo(std::shared_ptr<Character> target) = 0;
-    virtual void updateTargetsHealth(CharacterCoordinates src_character_coordinates, vector<CharacterCoordinates> grid_state) = 0;
-    void reload(); // raghad
+    virtual void updateTargetsHealth(std::vector<Character> characters) = 0;
+    void reload();
 
 public:
-    Character(Team team, units_t health, units_t ammo, units_t range, units_t power);
+    Character(mtm::Team team, mtm::units_t health, mtm::units_t ammo, mtm::units_t range, mtm::units_t power);
     ~Character() = default;
     Character(const Character&) = default;
     Character& operator=(const Character&) = default;
     clone();
+}
