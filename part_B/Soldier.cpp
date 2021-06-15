e97f4778a37e48cdac1749a2d0c36dbd9f3390f9
Sama Mazzawi <sama.mazz93@gmail.com>
2021-06-15 15:57:14 +0300

    Update Soldier.cpp

--- part_B/Soldier.cpp
+++ part_B/Soldier.cpp
@@ -1,21 +1,26 @@
 #include "Soldier.h"
 
+using std::shared_ptr;
 using namespace mtm;
 
-bool Soldier::attackInRange(GridPoint dst_grid_point) override {
+Soldier::Soldier(Team team, units_t health, units_t ammo, units_t range, units_t power, GridPoint coordinates) :
+                    team(team), health(health), ammo(ammo), range(range), power(power), coordinates(coordinates),
+                    motion_range(SOLDIER_MOTION_RANGE), load_addition(SOLDIER_LOAD_ADDITION) {}
+
+bool Soldier::attackInRange(shared_ptr<GridPoint> dst_grid_point) {
     if (GridPoint::distance(coordinates, dst_grid_point) > range) {
         return false;
     }
-    if (src_grid_point.x != dst_grid_point.x && src_grid_point.y != dst_grid_point.y) {
+    if (coordinates.row != dst_grid_point.row && coordinates.col != dst_grid_point.col) {
         return false
     }
     return true;
 }
 
-bool Soldier::enoughAmmo(Team dst_team) override {
+bool Soldier::enoughAmmo(Team dst_team) {
     return ammo >= 1;
 }
 
-bool legalTarget(Team dst_team) override {
+bool legalTarget(Team dst_team) {
     return true;
 }
