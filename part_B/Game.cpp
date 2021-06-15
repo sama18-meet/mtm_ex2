67a4adcb722f7fd5beed01df37baf8a53d7d72a5
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-06-15 15:27:24 +0300

    Update Game.cpp

--- part_B/Game.cpp
+++ part_B/Game.cpp
@@ -1,19 +1,37 @@
+#include "Game.h"
+#include <memory>
+#include <cstddef>
+
+using namespace mtm;
 
 
-
-
-Game::Game(int height, int width) height(height), width(width) {
+Game::Game(int height, int width) : height(height), width(width) {
+    if (heigth <=0 || width <=0){
+        throw IllegalArgument(); 
+    }
     grid = new char[height*width]; 
     memset(grid, ' ', height*width);
 }
-
 
 Game::~Game() {
     delete[] grid;
 }
 
-
-
-void Game::attack(const GridPoint & src_coordinates, const GridPoint & dst_coordinates) {
-    
+bool Game::isOver(Team* winningTeam=nullptr) const{
+    if(characters.size()==0){
+        return false;
+    }
+    std::vector<Character>::iterator i = characters.begin();     
+    Team first_character_team = i.getTeam;
+    for ( ; i != characters.end(); ++i)
+    {
+        if(i->.getTeam != first_character_team){
+            return false;
+        }
+    } 
+    if(winningTeam!=nullptr){
+    winningTeam=first_character_team;
+    }
+    return true;
 }
+
