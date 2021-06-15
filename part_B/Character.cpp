cd5e5972aeefe521b01d8ff725607d1b16c5a3d4
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-06-15 15:18:32 +0300

    Update Character.cpp

--- part_B/Character.cpp
+++ part_B/Character.cpp
@@ -1,6 +1,26 @@
+#include "Character.h"
+
+using namespace mtm;
+
+Character::Character(Team team, units_t health, units_t ammo, units_t range, units_t power, GridPint coordinates) : 
+                    team(team), health(health), ammo(ammo), range(range), power(power), coordinates(coordinates) {}
 
 
-
-bool Character::canMove(const GridPoint& src, const GridPoint& dst) {
-    
+Team Character::getTeam(){
+    return this->team;
+}
+units_t Character::getHealth(){
+    return this->health;
+}
+units_t Character::getAmmo(){
+    return this->ammo;
+}
+units_t Character::getRange(){
+    return this->range;
+}
+units_t Character::getPower(){
+    return this->power;
+}
+GridPoint Character::getCoordinates(){
+    return this->coordinates;
 }
